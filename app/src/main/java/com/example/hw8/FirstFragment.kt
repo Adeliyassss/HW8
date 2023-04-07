10c390eb2e5a215675232c5023e0ac32c8b65892
Adeliyassss <100111844+Adeliyassss@users.noreply.github.com>
2023-04-07 17:52:48 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw8/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.example.hw8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bundle = Bundle()
+        bundle.putString("key","text")
+        findNavController().navigate(R.id.action_firstFragment_to_secondFragment22,bundle)
+    }
+    
+}
